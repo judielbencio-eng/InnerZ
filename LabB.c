7640af19adf38f7a7ac47ac1ba8254c7ad3761f7
Judiel Bencio <judielbencio@gmail.com>
2025-11-15 19:55:48 +0800

    dgfgfg

---
+++ LabB.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//JUDIEL PEARLY G. BENCIO DIT 1-2
+
+int main(){
+    int saging = 100;
+    float mangga = 134.28;
+    char kean = 'j';
+
+    printf ("%d\n", saging);
+    printf ("%f\n", mangga);
+    printf ("%c\n", kean);
+
+    return 0;
+
+}
