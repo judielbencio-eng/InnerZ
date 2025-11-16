f779ae665d3cc17321ef2626edb2717c9636b0a0
Judiel Bencio <judielbencio@gmail.com>
2025-11-16 10:52:49 +0800

    added new file

---
+++ LabN.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//BENCIO, JUDIEL PEARLY G. DIT 1-2
+
+int main(){
+
+    int a = 1;
+
+        printf("WHILE LOOP\n");
+        while (a <=10){
+            printf ("%d\n", a);
+            a++;
+        }
+            printf("\n");
+
+    int b;
+        
+        printf("FOR LOOP\n");
+        for (b = 2; b <=20; b += 2){
+            printf("%d\n",b);
+        }
+            printf("\n");
+
+    int c = 1;
+        
+        printf("DO-WHILE LOOP\n");
+            do{
+                printf("%d\n",c);
+                c++;
+            }
+            while (c <= 5);
+
+
+
+            return 0;
+
+}
